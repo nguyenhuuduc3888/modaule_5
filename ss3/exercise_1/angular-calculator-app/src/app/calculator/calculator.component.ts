92943bd99fbe8db4ccf71e3995c84538fc9db2c3
nguyenhuuduc3888 <nguyenhuuduc3888@gmail.com>
2022-08-14 09:32:29 +0700

    up

---
+++ ss3/exercise_1/angular-calculator-app/src/app/calculator/calculator.component.ts
@@ -0,0 +1,40 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-calculator',
+  templateUrl: './calculator.component.html',
+  styleUrls: ['./calculator.component.css']
+})
+export class CalculatorComponent implements OnInit {
+  firstNumber = 0;
+  secondNumber = 0;
+  result = 0;
+  error = '';
+
+  calculator(operator) {
+    if (operator === '+') {
+      this.result = Number(this.firstNumber) + Number(this.secondNumber);
+      this.error = ' Ok';
+    } else if (operator === '-') {
+      this.result = Number(this.firstNumber) - Number(this.secondNumber);
+      this.error = ' Ok';
+    } else if (operator === '*') {
+      this.result = Number(this.firstNumber) * Number(this.secondNumber);
+      this.error = ' Ok';
+    } else {
+      if (this.secondNumber === 0) {
+        this.error = ' Không thành công,Không thể chia cho không';
+      } else {
+        this.result = Number(this.firstNumber) / Number(this.secondNumber);
+        this.error = ' Ok';
+      }
+    }
+  }
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
